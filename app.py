fa0734f7bd2fb88ad6a3968c91be2923b98bbd2f
Jay Hastings <jayhjman@gmail.com>
2020-11-24 09:36:36 -0800

    initial version of files

---
+++ app.py
@@ -0,0 +1,59 @@
+#
+# Hawaiian Climate
+#
+# A series of API calls to get various Hawaiian climate data
+#
+
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to the tables
+Measurements = Base.classes.measurement
+Stations = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+# Base route, API specs
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;"
+    )
+
+
+#################################################
+# Flask run main app 
+#################################################
+if __name__ == '__main__':
+    app.run(debug=True)
